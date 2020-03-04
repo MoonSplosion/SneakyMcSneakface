97d830a28fbff671f7d3b0aed283bac455fb2120
Zachary <Davidadallas25@live.com>
2020-03-04 12:21:11 -0700

    Added abstracts

---
+++ SneakyMcSneakface/Assets/Scripts/Pawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Pawn : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+    public virtual void Attack()
+    {
+        Debug.Log("Pawn Attack");
+    }
+    public virtual void Move()
+    {
+        Debug.Log("Move With Pawn movement behavior");
+    }
+}
